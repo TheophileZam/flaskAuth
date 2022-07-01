270b187a47197754b9965372d0704a656a1fc6b9
TheophileZam <theophile.zamolo@gmail.com>
2022-07-01 13:34:08 +0200

    test url

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from project import create_app,functions
+from flask_testing import LiveServerTestCase
 import pytest
 import random
 
@@ -8,3 +9,11 @@ def test_function():
     name = "Théophile"
     integ = random.randint(0,100)
     assert functions.format_name(name) == name + " du 62"
+
+
+class TestUserTakesTheTest(LiveServerTestCase):
+    def test_user_login(self):
+        # On ouvre le navigateur avec l'adresse du serveur.
+        self.driver.get(self.get_server_url())
+        # L'adresse dans l'url doit être celle que l'on attend.
+        assert self.driver.current_url == 'https://auth-deploiement.azurewebsites.net/'
